eaf96b033ecb8d9854aa253da1ebd53d0aaab8bd
Juan Sergio Pabón García <jpabgar0305_iesmarti@A2.2_PC16>
2023-02-01 13:23:30 +0100

    TEMA 6 AJAX

---
+++ Tema6/cargarDatos.js
@@ -0,0 +1,25 @@
+//Funciones de productos
+
+let inputForm = document.querySelector("#anadir");
+
+
+inputForm.addEventListener("click",anadirProductos);
+
+
+const cargarProductos = () =>{
+
+
+
+}
+
+const crearTablaProductos = () =>{
+
+
+
+}
+
+const anadirProductos = () => {
+
+
+
+}
